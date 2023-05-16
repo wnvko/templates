7219f49e09eeb188ff610dd684cf81327067063b
Milko Venkov <milko.venkov@gmail.com>
2023-05-16 17:30:20 +0300

    feat(WC): add select as cell editor

--- web-components/src/app/master-view/master-view.ts
+++ web-components/src/app/master-view/master-view.ts
@@ -1,7 +1,10 @@
 import '@infragistics/igniteui-webcomponents-grids/grids/combined.js';
 import { css, html, LitElement } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
+import { defineComponents, IgcSelectComponent } from 'igniteui-webcomponents';
 import DashboardDataService from '../service/dashboard-data-service';
+
+defineComponents(IgcSelectComponent);
 
 @customElement('app-master-view')
 export default class MasterView extends LitElement {
@@ -22,37 +25,41 @@ export default class MasterView extends LitElement {
   constructor() {
     super();
     const dashboardDataService: DashboardDataService = new DashboardDataService();
-      dashboardDataService.getAllTeamMembers().then(data => {
-        this.dashboardDataAllTeamMembers = data.map((e: any) => {
-          e.HireDate = new Date(e.HireDate);
-          return e;
-        })
-        }, err => console.log(err));
+    dashboardDataService.getAllTeamMembers().then((data: any) => {
+      this.dashboardDataAllTeamMembers = data.map((e: any) => {
+        e.HireDate = new Date(e.HireDate);
+        return e;
+      });
+    }, err => console.log(err));
   }
 
-  @property({type: Array})
+  @property({ type: Array })
   private dashboardDataAllTeamMembers?: any[];
 
   @query("cellEditor")
   private cellEditor?: HTMLInputElement;
 
-  public gridFirstNameHeaderTemplate = (ctx: any) => { // TODO: ctx: IgcColumnTemplateContext
-    return html`
+  public gridFirstNameHeaderTemplate = (ctx: any) => html`
       HT ${ctx.column?.field.toUpperCase()}
-      `;
-  }
+      `
 
-  public gridLastNameCellTemplate = (ctx: any) => { // TODO: ctx: IgcCellTemplateContext
-    return html`
+  public gridLastNameCellTemplate = (ctx: any) => html`
       CT ${ctx.cell?.value.toUpperCase()}
-      `;
-  }
+      `
 
-  public gridHireDataCellEditorTemplate = (ctx: any) => { // TODO: ctx: IgxCellEditorTemplateDirective
-    return html`
-        <input .value="${ctx.cell?.value}" @change="${(e: Event) => ctx.cell.value = (e.target as HTMLInputElement).value}"/>
-      `;
-  }
+  public gridHireDataCellEditorTemplate = (ctx: any) => html`
+        <input .value="${ctx.cell?.value}" @change="${(e: Event) => { ctx.cell.value = (e.target as HTMLInputElement).value; }}"/>
+      `
+
+  public gridFirstNameCellEditorTemplate = (ctx: any) => html`
+      <igc-select .value="${ctx.cell.editValue}" @igcChange="${(e: any) => { ctx.cell.value = e.detail.value; }}">
+        ${this.dashboardDataAllTeamMembers?.map((item: any) => html`
+          <igc-select-item value="${item.FirstName}">
+            ${item.FirstName}
+          </igc-select-item>
+        `)}
+      </igc-select>
+    `
 
   render() {
     return html`
@@ -61,8 +68,8 @@ export default class MasterView extends LitElement {
       <link rel='stylesheet' href='../../ig-theme.css'>
       <link rel='stylesheet' href='node_modules/@infragistics/igniteui-webcomponents-grids/grids/themes/light/material.css'>
       <igc-grid .data="${this.dashboardDataAllTeamMembers}" auto-generate="false" primary-key="FirstName" width="800px" height="600px" class="ig-typography grid">
-        <igc-column .headerTemplate="${this.gridFirstNameHeaderTemplate}" field="FirstName" groupable="true"></igc-column>
-        <igc-column .bodyTemplate="${this.gridLastNameCellTemplate}" field="LastName"></igc-column>
+        <igc-column field="FirstName" .headerTemplate="${this.gridFirstNameHeaderTemplate}" .inlineEditorTemplate="${this.gridFirstNameCellEditorTemplate}" editable="true"></igc-column>
+        <igc-column field="LastName" .bodyTemplate="${this.gridLastNameCellTemplate}"></igc-column>
         <igc-column field="HireDate" .inlineEditorTemplate="${this.gridHireDataCellEditorTemplate}" data-type="date" editable="true"></igc-column>
       </igc-grid>
     `;
