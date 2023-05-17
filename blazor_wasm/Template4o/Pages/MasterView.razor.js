8f6ffeca829b7d9f824011b20173492ef617b5fc
Milko Venkov <milko.venkov@gmail.com>
2023-05-17 17:49:27 +0300

    feat: Blazor edit template in JS with data passing .NET -> JS

--- blazor_wasm/Template4o/Pages/MasterView.razor.js
+++ blazor_wasm/Template4o/Pages/MasterView.razor.js
@@ -1,31 +1,26 @@
-﻿igRegisterScript("hireDateCellTemplate", (ctx) => {
+﻿let dashboardDataAllTeamMembers = [];
+
+igRegisterScript("hireDateCellTemplate", (ctx) => {
     return window.igTemplating.html`FAST CT ${ctx.cell.value}`
 }, false);
 igRegisterScript("hireDateCellEditTemplate", (ctx) => {
     return window.igTemplating.html`
-    <igx-input-group>
-        <input igxInput value="${ctx.cell?.value}" @change="${e => ctx.cell.value = e.target.value}" />
-    <igx-input-group>
+        <igc-input .value=${ctx.cell?.value} @igcChange="${e => ctx.cell.value = e.target.value}"></igc-input>
 `
 }, false);
 igRegisterScript("firstNameHeaderTemplate", (ctx) => {
     return window.igTemplating.html`HT ${ctx.column?.header}`
 }, false);
 igRegisterScript("firstNameInlineEditorTamplate", (ctx) => {
-    var items = [
-        { "FirstName": "Ann" },
-        { "FirstName": "Bob" },
-        { "FirstName": "Con" },
-        { "FirstName": "Dan" },
-    ]
-
     return window.igTemplating.html`
-        <igx-select>
-            <igx-select-item>Ann</igx-select-item>
-            <igx-select-item>Ann</igx-select-item>
-            <igx-select-item>Ann</igx-select-item>
+        <igc-select value=${ctx.cell?.value} @igcChange="${e => ctx.cell.value = e.detail.value}">
+            ${dashboardDataAllTeamMembers.map(item => window.igTemplating.html`
+                <igc-select-item value="${item.firstName}">${item.firstName}</igc-select-item>
+            `)}
         </igx-select>
 `
 }, false);
 
-/*<igx-select-item * ngFor="let item of items" value = "item.FirstName" > item.FirstName</igx - select - item >*/
+window.DashboardDataServiceGetAllTeamMembers = (dotNetHelper, items) => {
+    dashboardDataAllTeamMembers = items;
+}
