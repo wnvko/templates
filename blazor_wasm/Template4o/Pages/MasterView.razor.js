acf6bf844d1b5d9a60ac92b3d23dd0b276dca3be
Milko Venkov <milko.venkov@gmail.com>
2023-05-17 16:23:12 +0300

    feat: Blazor edit template in JS, WIP

--- blazor_wasm/Template4o/Pages/MasterView.razor.js
+++ blazor_wasm/Template4o/Pages/MasterView.razor.js
@@ -2,8 +2,30 @@
     return window.igTemplating.html`FAST CT ${ctx.cell.value}`
 }, false);
 igRegisterScript("hireDateCellEditTemplate", (ctx) => {
-    return window.igTemplating.html`<input value="${ctx.cell?.value}" @change="${e => ctx.cell.value = e.target.value}" />`;
+    return window.igTemplating.html`
+    <igx-input-group>
+        <input igxInput value="${ctx.cell?.value}" @change="${e => ctx.cell.value = e.target.value}" />
+    <igx-input-group>
+`
 }, false);
 igRegisterScript("firstNameHeaderTemplate", (ctx) => {
     return window.igTemplating.html`HT ${ctx.column?.header}`
 }, false);
+igRegisterScript("firstNameInlineEditorTamplate", (ctx) => {
+    var items = [
+        { "FirstName": "Ann" },
+        { "FirstName": "Bob" },
+        { "FirstName": "Con" },
+        { "FirstName": "Dan" },
+    ]
+
+    return window.igTemplating.html`
+        <igx-select>
+            <igx-select-item>Ann</igx-select-item>
+            <igx-select-item>Ann</igx-select-item>
+            <igx-select-item>Ann</igx-select-item>
+        </igx-select>
+`
+}, false);
+
+/*<igx-select-item * ngFor="let item of items" value = "item.FirstName" > item.FirstName</igx - select - item >*/
