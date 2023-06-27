7ae6febef68e68c2a5e945b5b1737fb30561fbc2
Milko Venkov <MVenkov@infragistics.com>
2023-06-27 08:43:07 +0300

    feat(WC): add select

--- web-components/src/app/master-view/master-view.ts
+++ web-components/src/app/master-view/master-view.ts
@@ -1,13 +1,13 @@
-import { html, css, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
 import { IgcCellTemplateContext } from '@infragistics/igniteui-webcomponents-grids/grids';
-import { defineComponents, IgcCheckboxComponent, IgcComboComponent, IgcInputComponent, IgcDateTimeInputComponent } from 'igniteui-webcomponents';
 import '@infragistics/igniteui-webcomponents-grids/grids/combined.js';
-import { Friend, Person, ProductsType } from '../static-data/northwind-model';
-import NorthwindService from '../service/Northwind-service';
+import { IgcCheckboxComponent, IgcComboComponent, IgcDateTimeInputComponent, IgcInputComponent, IgcSelectComponent, IgcSwitchComponent, defineComponents } from 'igniteui-webcomponents';
 import { IgcComboChangeEventArgs } from 'igniteui-webcomponents/components/combo/types';
+import { LitElement, css, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import NorthwindService from '../service/Northwind-service';
+import { Friend, Person, ProductsType } from '../static-data/northwind-model';
 
-defineComponents(IgcCheckboxComponent, IgcComboComponent, IgcInputComponent, IgcDateTimeInputComponent);
+defineComponents(IgcCheckboxComponent, IgcComboComponent, IgcDateTimeInputComponent, IgcInputComponent, IgcSelectComponent, IgcSwitchComponent);
 
 @customElement('app-master-view')
 export default class MasterView extends LitElement {
@@ -65,6 +65,16 @@ export default class MasterView extends LitElement {
     `;
   }
 
+  public selectInlineEditorTemplate = (ctx: IgcCellTemplateContext) => html`
+  <igc-select .value="${ctx.cell.editValue}" @igcChange="${(e: any) => ctx.cell.editValue = e.detail.value}" master_view-scope class="select">
+    ${this.people!.map(item => html`
+      <igc-select-item .value="${item.last_name}">
+        ${item.last_name}
+      </igc-select-item>
+    `)}
+  </igc-select>
+  `;
+
   public textInputInlineEditorTemplate = (ctx: IgcCellTemplateContext) => html`
     <igc-input type="text" .value="${ctx.cell.editValue}" @igcChange="${(e: CustomEvent<string>) => ctx.cell.editValue = e.detail}" master_view-scope class="input"></igc-input>
     `
@@ -77,16 +87,19 @@ export default class MasterView extends LitElement {
     <igc-date-time-input type="number" .value="${ctx.cell.editValue}" @igcChange="${(e: CustomEvent<Date | null>) => ctx.cell.editValue = e.detail}" master_view-scope class="input"></igc-date-time-input>
     `
 
+  public switchInlineEditorTemplate = (ctx: IgcCellTemplateContext) => html`
+		<igc-switch .checked="${ctx.cell.editValue}" @igcChange="${(e: any) => { ctx.cell.editValue = e.detail; }}" master_view-scope class="switch"></igc-switch>
+		`
   render() {
     return html`
       <link rel='stylesheet' href='../../ig-theme.css'>
       <link rel='stylesheet' href='node_modules/@infragistics/igniteui-webcomponents-grids/grids/themes/light/material.css'>
       <igc-grid .data="${this.people}" primary-key="id" row-editable="true" auto-generate="false" class="ig-typography ig-scrollbar grid">
         <igc-column field="first_name" data-type="string" .inlineEditorTemplate="${this.textInputInlineEditorTemplate}"></igc-column>
-        <igc-column field="last_name" data-type="string" ></igc-column>
+        <igc-column field="last_name" data-type="string" .inlineEditorTemplate="${this.selectInlineEditorTemplate}"></igc-column>
         <igc-column field="friends" .bodyTemplate="${this.comboBodyTemplate}" .inlineEditorTemplate="${this.comboInlineEditorTemplate}"></igc-column>
         <igc-column field="birthDay" data-type="date" .inlineEditorTemplate="${this.dateInputInlineEditorTemplate}"></igc-column>
-        <igc-column field="hasFriends" data-type="boolean"></igc-column>
+        <igc-column field="hasFriends" .inlineEditorTemplate="${this.switchInlineEditorTemplate}" data-type="boolean"></igc-column>
         <igc-column field="age" data-type="number".inlineEditorTemplate="${this.numberInputInlineEditorTemplate}"></igc-column>
       </igc-grid>
     `;
